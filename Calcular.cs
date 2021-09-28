613ec7c28b0e242c5a962f4782008656a9923664
AnaBetzaida <91514106+AnaBetzaida@users.noreply.github.com>
2021-09-28 15:05:27 -0500

    Add files via upload

---
+++ Calcular.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practica03WS.Code
+{
+    public class Calcular
+    {
+
+        public double Cuadrado(int B)
+        {
+            var result = B * B;
+            return result;
+        }
+
+        public double Triangulo(int T, int O)
+        {
+            var result = (T * O) / 2;
+            return result;
+        }
+
+        public double Circulo(int C)
+        {
+            var result = Math.PI * Math.Pow(C, 2);
+            return result;
+        }
+
+    }
+}
---
+++ CalcularWebService.asmx.cs
@@ -0,0 +1,43 @@
+﻿using Practica03WS.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Practica03WS
+{
+    /// <summary>
+    /// Descripción breve de CalcularWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalcularWebService : System.Web.Services.WebService
+    {
+        private Calcular Areas = new Calcular();
+        [WebMethod]
+        public double Cuadrado(int B)
+        {
+           
+            return Areas.Cuadrado(B);
+        }
+
+        [WebMethod]
+        public double AreaTriangulo(int T, int O)
+        {
+           
+            return Areas.Triangulo(T, O);
+        }
+
+        [WebMethod]
+        public double AreaCirculo(int C)
+        {
+         
+            return Areas.Circulo(C);
+        }
+
+    }
+}
